80613340dde17b33b76429a52dd84fc8d21484a5
Phil Lembo <phil.lembo@outlook.com>
2022-01-03 17:07:06 -0500

    Test func with multiple values; correct syntax error in fig 4-4 (result == None to result is None)

---
+++ code/04-test_find_root_approx.py
@@ -0,0 +1,36 @@
+def find_root(x, power, epsilon):
+    # Find interval containing answer
+    if x < 0 and power % 2 == 0:
+        return None  # Negative number has no even-powered roots
+    low = min(-1, x)
+    high = max(1, x)
+    # Use bisection search
+    ans = (high + low)/2
+    while abs(ans**power - x) >= epsilon:
+        if ans**power < x:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low)/2
+    return ans
+
+
+def test_find_root(x_vals, powers, epsilons):
+    for x in x_vals:
+        for p in powers:
+            for e in epsilons:
+                result = find_root(x, p, e)
+                if result is None:
+                    val = 'No root exists'
+                else:
+                    val = 'Okay'
+                    if abs(result**p - x) > e:
+                        val = 'Bad'
+                print(f'x = {x}, power = {p}, epsilon = {e}: {val}')
+
+
+x_vals = (0.25, 8, -8)
+powers = (1, 2, 3)
+epsilons = (0.1, 0.001, 1)
+
+test_find_root(x_vals, powers, epsilons)
